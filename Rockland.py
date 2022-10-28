8e45cf6092a5559a14635ffbfcd5eb3ff9675664
Jesse Cusack <jesse.cusack@oregonstate.edu>
2022-10-28 15:02:51 -0700

    loop over downloaded profiles

--- Rockland.py
+++ Rockland.py
@@ -714,20 +714,22 @@ class Download:
             if info is None: return
             # logging.info("INFO\n%s", json.dumps(info, sort_keys=True, indent=4))
 
+            with open("info.json", "w") as f: ### <<--- for debugging purpose, delete eventually
+                f.write(json.dumps(info, sort_keys=True, indent=4))
+
             save_dir = Path(args.directory)
             if not save_dir.exists():
                 logging.info("Creating output directory %s", save_dir)
                 save_dir.mkdir(parents=True)
 
-            filename = profile["name"]
-            nc_path = Path(save_dir, filename + ".nc")
-            logging.info("Saving profile to %s", nc_path)
+            # Loop over and save profiles
+            for i, key in enumerate(profiles):
+                filename = profiles[key]["name"]
+                nc_path = Path(save_dir, filename + ".nc")
+                logging.info("Saving profile to %s", nc_path)
 
-            ds = conv.profile_to_xrDataset(info["body"][0])
-            ds.to_netcdf()
-
-            # with open("info.json", "w") as f:
-            #     f.write(json.dumps(info, sort_keys=True, indent=4))
+                ds = conv.profile_to_xrDataset(info["body"][i])
+                ds.to_netcdf(nc_path)
 
 
     @staticmethod
